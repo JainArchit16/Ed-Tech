a6422970cd54fcdbb420e1ce7720e9d0ada51cb8
Archit <jainarchit1604@gmail.com>
2023-10-10 23:22:07 +0530

    Changed

--- Backend/controllers/Category.js
+++ Backend/controllers/Category.js
@@ -33,7 +33,7 @@ exports.createCategory=async (req,res)=>{
 }
 
 
-exports.showAllTags=async (req,res)=>{
+exports.showAllCategory=async (req,res)=>{
     try{
         const allTags=await Category.find({},{name:true,description:true});
         res.status(200).json({
