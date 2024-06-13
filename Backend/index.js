bea37a7f8440f6dc2502be210764125d125aed64
Archit <jainarchit1604@gmail.com>
2024-06-13 19:08:22 +0530

    changes for cors

--- Backend/index.js
+++ Backend/index.js
@@ -24,7 +24,10 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://study-notion-gamma-lovat.vercel.app/",
+    ],
     credentials: true,
   })
 );
