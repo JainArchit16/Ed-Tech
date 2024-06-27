f9fbdf4fb96b41d3c2dc091d1e000a417c350905
Archit <jainarchit1604@gmail.com>
2024-06-27 12:28:24 +0530

    Changes

--- Backend/middlewares/auth.js
+++ Backend/middlewares/auth.js
@@ -1,118 +1,89 @@
-const jwt=require("jsonwebtoken");
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
-const User=require("../models/User");
+const User = require("../models/User");
 
-
-// Glti yahan hogi agaar role mien chck krlena 
-
-
-exports.auth=async (req,res,next)=>{
-    try{
-
-    const token=req.body.token ||  req.cookies.token || req.header("Authorization").replace("Bearer ","");
-    if(!token)
-    {
-        return res.status(401).json({
-            message:"No token Found",
-            success:false,
-        })
+exports.auth = async (req, res, next) => {
+  try {
+    const token =
+      req.body.token ||
+      req.cookies.token ||
+      req.header("Authorization").replace("Bearer ", "");
+    if (!token) {
+      return res.status(401).json({
+        message: "No token Found",
+        success: false,
+      });
     }
     // console.log(token);
-    try{
-        let decode=jwt.verify(token,process.env.JWT_SECRET);
-        req.user=decode;
-    }
-    catch(err)
-    {
-        return res.status(401).json({
-            success:false,
-            message:"Token Not Verifed",
-        })
+    try {
+      let decode = jwt.verify(token, process.env.JWT_SECRET);
+      req.user = decode;
+    } catch (err) {
+      return res.status(401).json({
+        success: false,
+        message: "Token Not Verifed",
+      });
     }
     next();
+  } catch (error) {
+    console.error(error.message);
+    return res.status(401).json({
+      success: false,
+      message: "Something went Wrong in Auth",
+    });
+  }
+};
+
+exports.isStudent = (req, res, next) => {
+  try {
+    let role = req.user.role;
+    if (role !== "Student") {
+      return res.status(400).json({
+        message: "Protected Route for Students",
+        success: false,
+      });
     }
-    catch(error)
-    {
-        console.error(error.message);
-        return res.status(401).json({
-            success:false,
-            message:"Something went Wrong in Auth",
-            
-        })
+    next();
+  } catch (err) {
+    return res.status(500).json({
+      success: false,
+      message: "Something went Wrong in Student path",
+    });
+  }
+};
+
+exports.isAdmin = (req, res, next) => {
+  try {
+    let role = req.user.role;
+    if (role !== "Admin") {
+      return res.status(400).json({
+        message: "Protected Route for Admin",
+        success: false,
+      });
     }
-    
-}
+    next();
+  } catch (err) {
+    return res.status(500).json({
+      success: false,
+      message: "Something went Wrong in Admin path",
+    });
+  }
+};
 
-exports.isStudent=(req,res,next)=>{
-
-    try{
-
-        let role=req.user.role
-        if(role!=="Student")
-        {
-            return res.status(400).json({
-                message:"Protected Route for Students",
-                success:false,
-            })
-        }
-        next();
+exports.isInstructor = (req, res, next) => {
+  try {
+    let role = req.user.role;
+    if (role !== "Instructor") {
+      return res.status(400).json({
+        message: "Protected Route for Instructor",
+        success: false,
+      });
     }
-    catch(err)
-    {
-        return res.status(500).json({
-            success:false,
-            message:"Something went Wrong in Student path",
-        })
-    }
-   
-}
-
-
-exports.isAdmin=(req,res,next)=>{
-
-    try{
-
-        let role=req.user.role
-        if(role!=="Admin")
-        {
-            return res.status(400).json({
-                message:"Protected Route for Admin",
-                success:false,
-            })
-        }
-        next();
-    }
-    catch(err)
-    {
-        return res.status(500).json({
-            success:false,
-            message:"Something went Wrong in Admin path",
-        })
-    }
-   
-}
-
-
-exports.isInstructor=(req,res,next)=>{
-
-    try{
-
-        let role=req.user.role
-        if(role!=="Instructor")
-        {
-            return res.status(400).json({
-                message:"Protected Route for Instructor",
-                success:false,
-            })
-        }
-        next();
-    }
-    catch(err)
-    {
-        return res.status(500).json({
-            success:false,
-            message:"Something went Wrong in Instructor path",
-        })
-    }
-   
-}
+    next();
+  } catch (err) {
+    return res.status(500).json({
+      success: false,
+      message: "Something went Wrong in Instructor path",
+    });
+  }
+};
